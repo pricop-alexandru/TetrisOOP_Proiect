c84b23455a46ebb49ed6b42601ce5864a11e96af
pricop-alexandru <nophz506@gmail.com>
2023-10-31 20:56:11 +0200

    v0.1 - base grid created (day 1)

---
+++ Global.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Alex Pricop on 10/31/2023.
+//
+
+#pragma once
+
+constexpr unsigned char CELL_SIZE = 8;
+constexpr unsigned char COLUMNS = 10;
+constexpr unsigned char ROWS = 20;
+constexpr unsigned char SCREEN_RESIZE = 4;
+
+constexpr unsigned short FRAME_DURATION = 16667;
--- main.cpp
+++ main.cpp
@@ -1,61 +1,61 @@
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <chrono>
-#include <thread>
-
+#include<chrono>
+#include<random>
+#include<SFML/Graphics.hpp>
+#include"Global.hpp"
 #ifdef __linux__
 #include <X11/Xlib.h>
 #endif
 
-class SomeClass {
-public:
-    explicit SomeClass(int) {}
-};
+int main()
+{
+    unsigned lag = 0;
+    std::chrono::time_point<std::chrono::steady_clock> previous_time;
+    sf::Event event;
+    sf::RenderWindow window(sf::VideoMode( CELL_SIZE * COLUMNS * SCREEN_RESIZE, CELL_SIZE * ROWS * SCREEN_RESIZE), "Tetris", sf::Style::Close);
+    window.setView(sf::View(sf::FloatRect(0, 0,  CELL_SIZE * COLUMNS, CELL_SIZE * ROWS)));
+    previous_time = std::chrono::steady_clock::now();
+    while (1 == window.isOpen())
+    {
+        unsigned delta_time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - previous_time).count();
+        lag += delta_time;
+        while (FRAME_DURATION <= lag)
+        {
+            //Subtract the right thing from the left thing
+            lag -= FRAME_DURATION;
 
-SomeClass *getC() {
-    return new SomeClass{2};
-}
+            //Looping through the events
+            while (1 == window.pollEvent(event))
+            {
+                //Check the event type
+                switch (event.type)
+                {
+                    //If the user closed the game
+                    case sf::Event::Closed:
+                    {
+                        //Close the window
+                        window.close();
 
-int main() {
-    #ifdef __linux__
-    XInitThreads();
-    #endif
-
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;
-
-    sf::RenderWindow window;
-    // NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:30
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    window.setVerticalSyncEnabled(true);
-    //window.setFramerateLimit(60);
-
-    while(window.isOpen()) {
-        sf::Event e;
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                break;
-            default:
-                break;
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+            if (FRAME_DURATION > lag)
+            {
+                sf::RectangleShape cell(sf::Vector2f(CELL_SIZE-1,CELL_SIZE-1));
+                cell.setFillColor(sf::Color(255,0,0));
+                window.clear();
+                for(unsigned char a = 0; a < COLUMNS; a++)
+                {
+                    for(unsigned char b = 0; b < ROWS; b++)
+                    {
+                        cell.setPosition(CELL_SIZE * a, CELL_SIZE * b);
+                        window.draw(cell);
+                    }
+                }
+                window.display();
             }
         }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
-
-        window.clear();
-        window.display();
     }
-
-    return 0;
 }
